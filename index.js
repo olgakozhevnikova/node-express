de5ad2337ff1b86ece99bb8ad36746210eef5ba6
Olga Kozhevnikova <olga@starkflow.co>
2018-08-01 12:20:12 +0530

    Express Simple REST

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const http = require('http');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 const hostname = 'localhost';
 const port = 3000;
@@ -9,6 +10,51 @@ const port = 3000;
 const app = express();
 // using morgan
 app.use(morgan('dev'));
+app.use(bodyParser.json());
+
+app.all('/dishes', (req, res, next) => {
+  res.statusCode = 200;
+  res.setHeader('Content-type', 'text/plain');
+  // the next() looks for additional specifications which will matches 'dishes' endpoint
+  next();
+});
+
+// before get request happens, app.all will be executed first and req and res will be passed to the second parameter
+app.get('/dishes', (req, res) => {
+  res.end('Will send all the dishes to you');
+});
+
+app.post('/dishes', (req, res) => {
+  res.end('Will add the dish: ' + req.body.name + ' with details: ' + req.body.description);
+});
+
+app.put('/dishes', (req, res) => {
+  res.statusCode = 403;
+  res.end('PUT operation is not supported on /dishes');
+});
+
+app.delete('/dishes', (req, res) => {
+  res.end('Deleting all the dishes!');
+});
+
+// Actions for /:dishId endpoint
+app.get('/dishes/:dishId', (req, res) => {
+  res.end('Will send details of the dish: ' + req.params.dishId + ' to you!');
+});
+
+app.post('/dishes/:dishId', (req, res) => {
+  res.statusCode = 403;
+  res.end('POST operation is not supported on /dishes/' + req.params.dishId);
+});
+
+app.put('/dishes/:dishId', (req, res) => {
+  res.write('Updating the dish: ' + req.params.dishId);
+  res.end('Will update the dish: ' + req.body.name + ' with details: ' + req.body.description);
+});
+
+app.delete('/dishes/:dishId', (req, res) => {
+  res.end('Deleting dish: ' + req.params.dishId);
+});
 
 // (__dirname + '/public') informs express.static, that I will look at the folder 'public'
 // in the root folder of this project
