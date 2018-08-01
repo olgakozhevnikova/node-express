498af3a19ff3087b7e7d8cca1b9c415e3daf5f97
Olga Kozhevnikova <olga@starkflow.co>
2018-08-01 10:27:28 +0530

    Express serve static files

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 const express = require('express');
 const http = require('http');
+const morgan = require('morgan');
 
 const hostname = 'localhost';
 const port = 3000;
 
 // means that the application uses the express module
 const app = express();
+// using morgan
+app.use(morgan('dev'));
+
+// (__dirname + '/public') informs express.static, that I will look at the folder 'public'
+// in the root folder of this project
+// so this is the folder where static html files are served from by express server
+app.use(express.static(__dirname + '/public'));
 
 // next is used when we want to include an additional middleware
 app.use((req, res, next) => {
-  console.log(req.headers);
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
   res.end('<html><body><h1>This is an Express Server</h1></body></html>')
